88d5246bf0b3f4820b284ddc65a2fa4b29e3fcb9
Alexander Camenzind <camenal@usi.ch>
2016-05-03 18:25:20 +0200

    started to dovelop the minigame

--- core/src/ch/inf/usi/pf2/project/gameStates/MiniGame.java
+++ core/src/ch/inf/usi/pf2/project/gameStates/MiniGame.java
@@ -3,8 +3,13 @@ package ch.inf.usi.pf2.project.gameStates;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.math.Vector3;
+
+import ch.inf.usi.pf2.project.miniGameObjects.Boat;
 
 /**
  * Created by alexandercamenzind on 28/04/16.
@@ -14,12 +19,28 @@ public class MiniGame extends gameState {
     private OrthographicCamera cam;
     private Sprite bg;
 
+    private float WorldHeight;
+    private float WorldWidth;
+    private Boat boat;
+    private float aspectRatio;
+
 
     public MiniGame(SpriteBatch batch){
 
-        float aspectRatio = (float) Gdx.graphics.getWidth() / (float) Gdx.graphics.getHeight();
-        this.cam = new OrthographicCamera(2f * aspectRatio, 2f);
+
+        this.bg = new Sprite ( new Texture("miniGameBackground.png"));
         this.batch=batch;
+
+        WorldHeight=bg.getHeight();
+        WorldWidth=bg.getWidth();
+        aspectRatio = (float) Gdx.graphics.getWidth() / (float) Gdx.graphics.getHeight();
+        this.cam = new OrthographicCamera(WorldHeight * aspectRatio, WorldHeight);
+        cam.position.set((WorldHeight * aspectRatio)/2f,WorldHeight/2f,0);
+
+        boat = new Boat(new Sprite( new Texture("testBoat.png")),(WorldHeight * aspectRatio)/2f,WorldHeight/2f, WorldHeight);
+
+
+
     }
 
     @Override
@@ -34,7 +55,9 @@ public class MiniGame extends gameState {
 
         //this is where we can actually display stuff on the screen
         batch.begin();
-
+        bg.draw(batch);
+       // batch.draw(boat.getBoat(),boat.getPosition().x, boat.getPosition().y);
+        boat.getBoat().draw(batch);
         batch.end();
 
     }
@@ -43,10 +66,16 @@ public class MiniGame extends gameState {
 
     @Override
     public void update(float dt){
+        boat.updateBoat(dt);
+
+        cam.position.set(boat.getBoat().getX()+(WorldHeight * aspectRatio)/2f, WorldHeight / 2f, 0);
+
+
     }
 
     @Override
     public void inputHandler(){
+        boat.handleInput();
     }
 
 }
--- core/src/ch/inf/usi/pf2/project/miniGameObjects/Boat.java
+++ core/src/ch/inf/usi/pf2/project/miniGameObjects/Boat.java
@@ -1,13 +1,46 @@
 package ch.inf.usi.pf2.project.miniGameObjects;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.math.Vector2;
 
 /**
  * Created by alexandercamenzind on 03/05/16.
  */
 public class Boat {
-    private Texture boat;
+    private Sprite boat;
     private Vector2 velocity;
-    private Vector2 position;
+    private float worldHeight;
+
+
+
+    public Boat(Sprite pic, float pos_x, float pos_y, float worldHeight){
+        boat = pic;
+        float ratio = boat.getWidth() / boat.getHeight();
+        //boat.setScale((worldHeight/5f)*ratio,worldHeight/5f);
+        //boat.scale(worldHeight/5f);
+        boat.setSize((worldHeight/5f)*ratio,worldHeight/5f);
+
+
+
+        velocity = new Vector2(100,0);
+        boat.setPosition(pos_x-boat.getWidth()/2f,pos_y-boat.getHeight()/2f);
+
+    }
+
+    public Sprite getBoat() {
+        return boat;
+    }
+
+    public void updateBoat(float dt){
+        boat.translate(velocity.x * dt,-velocity.y * dt);
+    }
+
+    public void handleInput(){
+        if(Gdx.input.isTouched()){
+            velocity.set(Gdx.input.getX(),Gdx.input.getY()-worldHeight/2f);
+        }
+    }
+
 }
