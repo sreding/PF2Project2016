1f6c7e92f0b6ac2a75ebccd46148e6465193ff06
sreding <redins@usi.ch>
2016-05-18 23:48:04 +0200

    forgot to add some stuff and alex is really angry

--- core/src/ch/inf/usi/pf2/project/mapObjects/Port.java
+++ core/src/ch/inf/usi/pf2/project/mapObjects/Port.java
@@ -4,4 +4,17 @@ package ch.inf.usi.pf2.project.mapObjects;
  * Created by simonreding on 18/05/16.
  */
 public class Port {
+    private int x;
+    private int y;
+    private String name;
+
+    public Port(String name, int x, int y){
+        this.name = name;
+        this.x = x;
+        this.y = y;
+    }//constructor
+
+    public String getName() {
+        return name;
+    }
 }
