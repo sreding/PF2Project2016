59a1040dc380a7e210f0422d10b9e905b6fd3975
sreding <redins@usi.ch>
2016-05-23 18:26:36 +0200

    added function to create simple articles in news

---
+++ core/src/ch/inf/usi/pf2/project/mapObjects/Article.java
@@ -0,0 +1,26 @@
+package ch.inf.usi.pf2.project.mapObjects;
+import
+
+import com.badlogic.gdx.scenes.scene2d.Actor;
+
+/**
+ * Created by simonreding on 23/05/16.
+ */
+
+public class Article extends Actor {
+    private String title;
+    private String content;
+
+    public Article(String title,String content){
+        this.title = title;
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+}
--- core/src/ch/inf/usi/pf2/project/mapObjects/News.java
+++ core/src/ch/inf/usi/pf2/project/mapObjects/News.java
@@ -20,7 +20,11 @@ import com.badlogic.gdx.utils.Align;
 import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.Pool;
 import com.badlogic.gdx.utils.Pools;
+import com.sun.org.apache.bcel.internal.generic.NEW;
 
+
+import java.util.ArrayList;
+import java.util.Random;
 /**
  * Created by simonreding on 23/05/16.
  */
@@ -30,6 +34,31 @@ public class News extends WidgetGroup {
     static public Color debugTableColor = new Color(0, 0, 1, 1);
     static public Color debugCellColor = new Color(1, 0, 0, 1);
     static public Color debugActorColor = new Color(0, 1, 0, 1);
+
+    private ArrayList articles;
+    public News() {
+        this.articles = new ArrayList<Article>();
+    }
+
+    //simple function that generates a list of articles given lists of
+    // strings containing names of regions and desasters
+    private static ArrayList<Article> makeArticles(ArrayList<String> regions,
+                                                   ArrayList<String> events,
+                                                   ArrayList<Article> finishedArticles, int n) {
+        if(events.size() == n){
+            return finishedArticles;
+    }else{
+            Random rn = new Random();
+            String country = regions.get(rn.nextInt(regions.size()+1));
+            String title = events.get(n)+ " in " +country;
+            String text = "The" + events.get(n) + " that destroyed " + regions+"appear to" +
+                    "have devastationg consequences to the region";
+            finishedArticles.add(new Article(title,text));
+            n++;
+            return makeArticles(regions,events,finishedArticles,n);
+        }
+
+    }
 
 
 
