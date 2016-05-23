5cf7e215944c2d368f662c4fb39a81a8e4896034
sreding <redins@usi.ch>
2016-05-23 14:43:01 +0200

    added news placeholder and cinzel regular font

---
+++ core/src/ch/inf/usi/pf2/project/mapObjects/News.java
@@ -0,0 +1,37 @@
+package ch.inf.usi.pf2.project.mapObjects;
+
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.Touchable;
+import com.badlogic.gdx.scenes.scene2d.ui.Label.LabelStyle;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Value;
+import com.badlogic.gdx.scenes.scene2d.ui.WidgetGroup;
+import com.badlogic.gdx.scenes.scene2d.ui.Value.Fixed;
+import com.badlogic.gdx.scenes.scene2d.ui.Cell;
+import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
+import com.badlogic.gdx.scenes.scene2d.utils.Layout;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.utils.Align;
+import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.Pool;
+import com.badlogic.gdx.utils.Pools;
+
+/**
+ * Created by simonreding on 23/05/16.
+ */
+
+
+public class News extends WidgetGroup {
+    static public Color debugTableColor = new Color(0, 0, 1, 1);
+    static public Color debugCellColor = new Color(1, 0, 0, 1);
+    static public Color debugActorColor = new Color(0, 1, 0, 1);
+
+
+
+
+    }
