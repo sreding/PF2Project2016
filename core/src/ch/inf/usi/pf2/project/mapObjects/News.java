32e7eeabdf9ff545f2b4be3f6ed8581d6d7b2b81
sreding <redins@usi.ch>
2016-05-28 11:08:07 +0200

    added simple background texture to be changed for final version

--- core/src/ch/inf/usi/pf2/project/mapObjects/News.java
+++ core/src/ch/inf/usi/pf2/project/mapObjects/News.java
@@ -1,5 +1,11 @@
 package ch.inf.usi.pf2.project.mapObjects;
 
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
@@ -10,36 +16,17 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
-import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Touchable;
 import com.badlogic.gdx.scenes.scene2d.ui.*;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Button;
-import com.badlogic.gdx.scenes.scene2d.ui.Label.LabelStyle;
-import com.badlogic.gdx.scenes.scene2d.ui.Value.Fixed;
-import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
-import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
-import com.badlogic.gdx.scenes.scene2d.utils.Layout;
-import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
-import com.badlogic.gdx.scenes.scene2d.utils.TiledDrawable;
 import com.badlogic.gdx.utils.Align;
-import com.badlogic.gdx.utils.Array;
-import com.badlogic.gdx.utils.Pool;
-import com.badlogic.gdx.utils.Pools;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
-import com.badlogic.gdx.utils.viewport.Viewport;
-import com.sun.org.apache.bcel.internal.generic.NEW;
-import com.sun.org.apache.xalan.internal.xsltc.compiler.util.TestGenerator;
-
-
 import java.util.ArrayList;
 import java.util.Random;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import ch.inf.usi.pf2.project.gameStates.GameState;
-import javafx.scene.control.TextFormatter;
-import javafx.scene.layout.Background;
-import sun.security.x509.AlgIdDSA;
 
 /**
  * Created by simonreding on 23/05/16.
@@ -61,18 +48,19 @@ public class News extends GameState {
     private Label label;
     private Table header;
     private ButtonGroup buttonGroup;
-    private Sprite background;
-    private SpriteBatch batch;
-    private BitmapFont bitmapFont;
+    //private Sprite background;
+    //private SpriteBatch batch;
+    //private BitmapFont bitmapFont;
+    private Sprite backgroundImage;
 
 
 
 
     public News(SpriteBatch batch) {
-        bitmapFont = new BitmapFont(Gdx.files.internal("cinzel45.fnt"),Gdx.files.internal("cinzel45.png"),false);
-        this.spriteBatch = batch;
-        background = new Sprite(new Texture(Gdx.files.internal("backgroundTexture.png")));
-
+        //bitmapFont = new BitmapFont(Gdx.files.internal("cinzel45.fnt"),Gdx.files.internal("cinzel45.png"),false);
+        spriteBatch = new SpriteBatch();
+        backgroundImage = new Sprite(new Texture(Gdx.files.internal("backgroundTexture.png")),Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
+        backgroundImage.setColor(1f,0f,0.4f,1f);
         this.articles = new ArrayList<Article>();
         stage = new Stage(new ScreenViewport(),batch);
         skin = new Skin(Gdx.files.internal("uiskin.json"));//,Gdx.files.internal("uiskin.atlas"));
@@ -130,12 +118,12 @@ public class News extends GameState {
         Label news = new Label("NEWS",skin);
         news.setFontScale(Gdx.graphics.getWidth()*4/1810f,Gdx.graphics.getHeight()*4/1080f);
         //news.setFontScale(4);
-        news.setColor(0.6f,0.5f,0.6f,1f);
+        news.setColor(1f,1f,1f,1f);
         header.add(news).align(Align.center);
         Label headlines = new Label("Headlines",skin);
         headlines.setFontScale(Gdx.graphics.getWidth()*2/1810f,Gdx.graphics.getHeight()*2/1080f);
         //headlines.setFontScale(2f);
-        headlines.setColor(0.6f,0.5f,0.6f,1f);
+        headlines.setColor(1f,1f,1f,1f);
         table.add(header).align(Align.center);
         table.add(headlines).align(Align.center);
         table.row();
@@ -165,8 +153,13 @@ public class News extends GameState {
             // IMPLEMENT GOING BACK TO MAP
         }
 
+
         stage.act(Gdx.graphics.getDeltaTime());
+        spriteBatch.begin();
+        backgroundImage.draw(spriteBatch);
+        spriteBatch.end();
         stage.draw();
+
     }
 
 
@@ -179,6 +172,7 @@ public class News extends GameState {
         for(Article a:newA){
             TxtButton button = a.getTextButton();
             button.setIndex(numberOfArticles);
+            button.getLabel().setFontScale(Gdx.graphics.getWidth()*4/1810f,Gdx.graphics.getHeight()*4/1080f);
             button = autoPad(button, 3);
             verticalGroup.addActor(button);
             this.articles.add(a);
