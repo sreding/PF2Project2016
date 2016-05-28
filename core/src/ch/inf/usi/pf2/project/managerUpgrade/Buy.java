254c3d9285911b609a114d707bc91d3afdf964dd
ilijagjogjiev <gjorgi@usi.ch>
2016-05-28 17:34:50 +0200

    dsada

---
+++ core/src/ch/inf/usi/pf2/project/managerUpgrade/Buy.java
@@ -0,0 +1,15 @@
+package ch.inf.usi.pf2.project.managerUpgrade;
+
+/**
+ * Created by ilijagjorgjiev on 5/27/16.
+ */
+public class Buy {
+    private int price;
+
+    public Buy(int price)
+    {
+        this.price = price;
+    }
+
+    public int getPrice(){ return price;}
+}
